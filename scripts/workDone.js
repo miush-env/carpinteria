17b4d05a76f0d906bfe8c2811ac04e1d9d36b459
miush_env <basach1582@gmail.com>
2026-01-25 16:01:42 -0300

    Reutilizacion del codigo para el menu desplegable
    copi y pega de las lineas de codigo de otro archivo javaScript local del
    proyecto

--- scripts/workDone.js
+++ scripts/workDone.js
@@ -135,3 +135,17 @@ modal.onclick =  e => {
 };
 
 // menu desplegable
+  const header = document.querySelector('.header-nav-bar');
+  const toggle = document.querySelector('.menu-toggle');
+  const links = document.querySelectorAll('.nav-bar a');
+
+  toggle.addEventListener('click', () => {
+    header.classList.toggle('active');
+  });
+
+  links.forEach(link =>
+    link.addEventListener('click', () => {
+      header.classList.remove('active');
+    })
+  );
+
